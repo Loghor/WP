f3f6aa429527b3a30727d486d96387090ed2bf16
Loghor <Loghor@gmail.com>
2013-12-16 15:41:05 +0100

    Dodanie klasy ModelEvaluation odpowiedzialnej za liste wszystkich ocen. Dodawkowo wprowadzony Data Binding dla obiektu ListPicker w SecondPage.

--- WP_1/WP_1/SecondPage.xaml.cs
+++ WP_1/WP_1/SecondPage.xaml.cs
@@ -7,17 +7,24 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Data;
+using System.ComponentModel;
 
 namespace WP_1
 {
     public partial class SecondPage : PhoneApplicationPage
     {
-        String[] EvaluationItem = {"2","2.5","3","3.5","4","4.5","5"};
-        String EvaluationName = "Ocena";
+        public ModelEvaluation _model;
+
         public SecondPage()
         {
             InitializeComponent();
-            this.lpkEvaluation.ItemsSource = EvaluationItem;
+            _model = new ModelEvaluation()
+            {
+                EvaluationItems = new string[]{"2","2.5","3","3.5","4","4.5","5"}
+            };
+
+            this.DataContext = _model;
         }
     }
 }
