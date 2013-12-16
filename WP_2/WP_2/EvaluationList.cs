a1a81ec04491a14fb401d954a8bff00f3bc3e897
Loghor <Loghor@gmail.com>
2013-12-16 18:54:35 +0100

    Utworzenie klasy EvaluationList ktora przechowywac bedzie informacje pojedynczym studencie (imie, nazwisko, ocena). Rowniez w MainPage zostal skonstruowany ListBox (nie mam pewnosci czy dziala poniewaz nie dodawalem wpisow!!!)

---
+++ WP_2/WP_2/EvaluationList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP_2
+{
+    class EvaluationList
+    {
+        private string _name;
+        private string _surname;
+        private string _evaluation;
+
+        public EvaluationList(string name, string surname, string evaluation)
+        {
+            _name = name;
+            _surname = surname;
+            _evaluation = evaluation;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+
+        public string Evaluation
+        {
+            get
+            {
+                return _evaluation;
+            }
+            set
+            {
+                _evaluation = value;
+            }
+        }
+    }
+}
